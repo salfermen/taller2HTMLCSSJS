04d11a6362f2c09d3814e4cefe879da4c7a3b6e2
salfermen <salimferezmendez2005@gmail.com>
2025-02-25 15:23:49 -0500

    fix image and url

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,12 @@ function nuevoParrafo() {
 
 function nuevoEnlace() {
     var contenido = document.getElementById('contenido');
-    var url = prompt("Ingrese la URL del enlace:");
-    var texto = document.getElementById("texto").value;
-    if (url) {
+    var texto = document.getElementById("enlaceTexto").value;
+    var url = document.getElementById("enlaceURL").value;
+    if (texto && url) {
         contenido.innerHTML += '<a href="' + url + '" target="_blank">' + texto + '</a><br>';
+    } else {
+        alert("Por favor, ingresa el texto y la URL del enlace.");
     }
 }
 
@@ -23,9 +25,9 @@ function cargarImagen(event) {
     var archivos = event.target.files;
     if (FileReader && archivos && archivos.length) {
         var imagen = new FileReader();
-        imagen.onload = function() {
+        imagen.onload = function(e) {
             var etiquetaImg = document.createElement("img");
-            etiquetaImg.setAttribute('src', imagen.result);
+            etiquetaImg.setAttribute('src', e.target.result);
             etiquetaImg.setAttribute('width', '200px');
             etiquetaImg.setAttribute('height', '180px');
             document.getElementById('contenido').appendChild(etiquetaImg);
